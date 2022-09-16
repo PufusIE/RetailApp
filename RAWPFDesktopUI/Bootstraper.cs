3f6c61806e248f7e552dfab762aeefc50073dd72
Yaroslav Mechenko <mechenko.yaroslav@gmail.com>
2022-09-16 23:03:42 +0100

    Add Dependency Injection
    Adds Simple Container Dependency Injection system.

--- RAWPFDesktopUI/Bootstraper.cs
+++ RAWPFDesktopUI/Bootstraper.cs
@@ -3,6 +3,7 @@ using RAWPFDesktopUI.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,14 +12,46 @@ namespace RAWPFDesktopUI
 {
     internal class Bootstraper : BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
         public Bootstraper()
         {
             Initialize();
         }
+
+        //dependency injection
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                 .Singleton<IWindowManager, WindowManager>()
+                 .Singleton<IEventAggregator, EventAggregator>();
+
+            //reflection
+            GetType().Assembly.GetTypes()
+                 .Where(type => type.IsClass)
+                 .Where(type => type.Name.EndsWith("ViewModel"))
+                 .ToList()
+                 .ForEach(viewModelType => _container.RegisterPerRequest(
+                     viewModelType, viewModelType.Name, viewModelType));
+        }
+
         //diplaying main window and wiring up mvvm arch
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewForAsync<ShellViewModel>();
+        }
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
