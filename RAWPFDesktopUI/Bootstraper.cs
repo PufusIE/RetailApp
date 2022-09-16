4ca04413abd967f4e4b1886b5fbc28cbc2d27798
Yaroslav Mechenko <mechenko.yaroslav@gmail.com>
2022-09-16 22:52:40 +0100

    Add WPF Project with MVVM
    Adds WPF user interface project and configures it
    with Caliburn Micro and Bootstraper for MVVM framework.

---
+++ RAWPFDesktopUI/Bootstraper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using RAWPFDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RAWPFDesktopUI
+{
+    internal class Bootstraper : BootstrapperBase
+    {
+        public Bootstraper()
+        {
+            Initialize();
+        }
+        //diplaying main window and wiring up mvvm arch
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+    }
+}
