edb633e17163390e650e83c4ddc8217ab3ce06b4
Yaroslav Mechenko <mechenko.yaroslav@gmail.com>
2022-10-31 23:37:49 +0000

    Add register page to Blazor.

--- Portal/Program.cs
+++ Portal/Program.cs
@@ -24,6 +24,6 @@ builder.Services.AddTransient<IProductEndPoint, ProductEndPoint>();
 builder.Services.AddTransient<IUserEndpoint, UserEndpoint>();
 builder.Services.AddTransient<ISaleEndpoint, SaleEndpoint>();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
--- RAWPFDesktopUILibrary/Models/CreateUserModel.cs
+++ RAWPFDesktopUILibrary/Models/CreateUserModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
@@ -8,22 +9,25 @@ namespace RAWPFDesktopUILibrary.Models
     public class CreateUserModel
     {
         [Required]
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
 
         [Required]
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
 
         [Required]
         [EmailAddress]
+        [DisplayName("Email Address")]
         public string EmailAddress { get; set; }
 
         [Required]
-        [RegularExpression(@"[A-Z][a-z][0-9]|.{8,}", 
-        ErrorMessage = "Password must be at least 8 characters long and contain number, uppercase and lowercase character")]
+        // TODO: fix - [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$", ErrorMessage = "Password must be at least 8 characters long and contain number, uppercase and lowercase character")]
         public string Password { get; set; }
 
         [Required]
         [Compare(nameof(Password), ErrorMessage ="The passwords do not match")]
+        [DisplayName("Confirm Password")]
         public string ConfirmPassword { get; set; }
     }
 }
