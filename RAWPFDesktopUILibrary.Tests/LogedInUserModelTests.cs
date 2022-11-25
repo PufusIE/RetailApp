e730b02aca0838d760007da1daf0081041855068
Yaroslav Mechenko <mechenko.yaroslav@gmail.com>
2022-11-25 21:51:57 +0000

    Add Unit Tests to UI Library.

---
+++ RAWPFDesktopUILibrary.Tests/LogedInUserModelTests.cs
@@ -0,0 +1,44 @@
+﻿using RAWPFDesktopUILibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAWPFDesktopUILibrary.Tests
+{
+    public class LogedInUserModelTests
+    {
+        [Fact]
+        public void ResetUserModel_ShouldResetLoggedInUserModel()
+        {
+            var expected = new LoggedInUserModel
+            {
+                Token = "",
+                Id = "",
+                FirstName = "",
+                LastName = "",
+                EmailAddress = "",
+                CreateDate = DateTime.MinValue
+            };
+
+            var actual = new LoggedInUserModel
+            {
+                Token = "1",
+                Id = "1",
+                FirstName = "a",
+                LastName = "a",
+                EmailAddress = "a",
+                CreateDate = DateTime.Now
+            };
+            actual.ResetUserModel();
+
+            Assert.Equal(expected.Token, actual.Token);
+            Assert.Equal(expected.Id, actual.Id);
+            Assert.Equal(expected.FirstName, actual.FirstName);
+            Assert.Equal(expected.LastName, actual.LastName);
+            Assert.Equal(expected.EmailAddress, actual.EmailAddress);
+            Assert.Equal(expected.CreateDate, actual.CreateDate);
+        }
+    }
+}
---
+++ RAWPFDesktopUILibrary.Tests/UserModelTests.cs
@@ -0,0 +1,33 @@
+﻿
+using RAWPFDesktopUILibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAWPFDesktopUILibrary.Tests
+{
+    public class UserModelTests
+    {
+        [Fact]
+        public void RoleList_ShouldReturnExpectedValue()
+        {
+            string expected = "Admin, Manager";
+
+            var model = new UserModel { Roles = CreateDictionarry() };
+            var actual = model.RoleList;
+
+            Assert.Equal(expected, actual);
+        }         
+
+        private static Dictionary<string, string> CreateDictionarry()
+        {
+            var output = new Dictionary<string, string>(); 
+            output.Add("123", "Admin"); 
+            output.Add("234", "Manager");
+
+            return output;
+        }
+    }
+}
